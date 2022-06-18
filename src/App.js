a832472ffe93e1b2aec1d9a05f164f830ff85fb8
Mridul <mridulpal19@gmail.com>
2022-06-19 00:34:34 +0530

    add total price

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ class App extends React.Component {
         {price:14999,
         title:'Phone',
         qty:1,
-        img:'',
+        img:'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
     id:1},
         {price:899,
         title:'watch',
         qty:5,
-        img:'',
+        img:'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
         id:2},
         {price:89999,
         title:'Laptop',
          qty:1,
-        img:'',
+        img:'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80',
         id:3}
        ]
 
@@ -69,6 +69,15 @@ getCartCount=()=>{
   });
   return count;
 }
+getCartTotal=()=>{
+  const {products}=this.state;
+  let count=0;
+  products.forEach(product => {
+    count +=product.price*product.qty;
+    
+  });
+  return count;
+}
   render(){
     const {products} = this.state
   return (
@@ -80,6 +89,8 @@ getCartCount=()=>{
       onIncreaseQuantity={this.handleIncreaseQuantity} 
       onDecreaseQuantity={this.handleDecreaseQuantity}
       onHandleDeleteProduct={this.handleDeleteProduct} />
+      <div style={{font:20,padding:20}}>Total :{this.getCartTotal()}</div>
+    
     </div>
   );
 }
